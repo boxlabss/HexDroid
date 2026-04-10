417f46dbc7606120e814cbc74765384df9a9458f
J Samuel <j.samuel@vanquishsolutions.co.uk>
2026-04-10 05:00:55 +0100

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.boxlabs.hexdroid"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.5.8"
+        versionCode = 15
+        versionName = "1.5.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
