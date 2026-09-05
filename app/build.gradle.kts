0ce0d232c38dcf11993198b0e92ca176162ed386
J Samuel <j.samuel@vanquishsolutions.co.uk>
2026-09-05 23:40:49 +0100

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.boxlabs.hexdroid"
         minSdk = 26
         targetSdk = 37
-        versionCode = 31
-        versionName = "1.7.3"
+        versionCode = 32
+        versionName = "1.7.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     // Release signing
